c5e433a4cd2419c7f8d7aa876508db7759aabec3
Griffin-Lane <Griffin_Lane@mckinsey.com>
2023-07-21 15:18:56 -0400

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 
 #!/bin/bash
 
-gunicorn -k uvicorn.workers.UvicornWorker --port $WEBSITES_PORT app:app
+#gunicorn -k uvicorn.workers.UvicornWorker --port $WEBSITES_PORT app:app
+gunicorn --port $WEBSITES_PORT app:app
