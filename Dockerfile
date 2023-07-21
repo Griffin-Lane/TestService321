1992b7c606c9a8b4cd20c03628fdecbc89443e45
Griffin-Lane <Griffin_Lane@mckinsey.com>
2023-07-21 15:32:50 -0400

    .

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-EXPOSE 8000 $PORT $WEBSITES_PORT
+EXPOSE $PORT
 
 ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@
 #!/bin/bash
 
 #gunicorn -k uvicorn.workers.UvicornWorker --port $WEBSITES_PORT app:app
-gunicorn --port $PORT app:app
+#gunicorn --port $PORT app:app
+
+gunicorn --bind=0.0.0.0 --timeout 600 app:app
