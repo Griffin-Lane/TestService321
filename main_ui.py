d34fd8e9851e747ea868b60f7ce389bad58e5754
Griffin-Lane <Griffin_Lane@mckinsey.com>
2023-07-21 11:02:57 -0400

    new main_py

--- main_ui.py
+++ main_ui.py
@@ -19,105 +19,105 @@ assert GENAI_DATA_ASK_API_ENDPOINT != None
 from pydantic import BaseModel
 
 
-class Source(str, Enum):
-    email = "email"
-    file = "file"
-    chat = "chat"
-    sql = "sql"
+# class Source(str, Enum):
+#     email = "email"
+#     file = "file"
+#     chat = "chat"
+#     sql = "sql"
 
-class Metadata(BaseModel):
-    source: Optional[Source] = None
-    source_id: Optional[str] = None
-    url: Optional[str] = None
-    created_at: Optional[str] = None
-    author: Optional[str] = None
-    database: Optional[str] = None
-    tables: Optional[str] = None
-    sql: Optional[str] = None
+# class Metadata(BaseModel):
+#     source: Optional[Source] = None
+#     source_id: Optional[str] = None
+#     url: Optional[str] = None
+#     created_at: Optional[str] = None
+#     author: Optional[str] = None
+#     database: Optional[str] = None
+#     tables: Optional[str] = None
+#     sql: Optional[str] = None
 
-class ChunkWithMetadata(BaseModel):
-    text: str
-    metadata: Metadata
+# class ChunkWithMetadata(BaseModel):
+#     text: str
+#     metadata: Metadata
 
-    def format_with_metadata(self) -> str:
-        if self.metadata.tables:
-            return f"{self.metadata.tables}:{self.text}"
-        return f"{self.metadata.source_id}:{self.text}"
+#     def format_with_metadata(self) -> str:
+#         if self.metadata.tables:
+#             return f"{self.metadata.tables}:{self.text}"
+#         return f"{self.metadata.source_id}:{self.text}"
 
-class Ask(BaseModel):
-    question: str
+# class Ask(BaseModel):
+#     question: str
 
-class Answer(BaseModel):
-    content: str
-    metadata: List[ChunkWithMetadata]
-
-
-def dispatch_payload(payload: Dict[str, Any]) -> Answer:
-    headers: Dict[str, str] = {
-        "Authorization": f"Bearer {BEARER_TOKEN}"
-    }
-    #url: str = f"{GENAI_DATA_ASK_API_ENDPOINT}/ask"
-    url: str = f"{GENAI_DATA_ASK_API_ENDPOINT}/ask?structured=true"
-    response: requests.Response = requests.post(url=url, headers=headers, json=payload)
-    status_code = response.status_code
-    content = response.json()
-    print(f"status_code: {status_code}")
-    print(f"content:")
-    print(json.dumps(content, indent=4))
-    ask = content.get('answer')
-    return Answer(**ask)
-
-def create_payload(question: str) -> Dict[str, Any]:
-
-    payload: Dict[str, Any] = {
-        "ask": {
-            "question": question
-        }
-    }
-    return payload
+# class Answer(BaseModel):
+#     content: str
+#     metadata: List[ChunkWithMetadata]
 
 
-def ask(query: str):
-    payload = create_payload(query)
-    response: Answer = dispatch_payload(payload)
-    metadata: List[ChunkWithMetadata] = response.metadata
-    # output = ''
-    # for m in metadata:
-    #     output = output + str(m) + "\n"
-    #     #m.metadata.<author....>
-    print(response.content)
-    #print(output)
-    return response.content
+# def dispatch_payload(payload: Dict[str, Any]) -> Answer:
+#     headers: Dict[str, str] = {
+#         "Authorization": f"Bearer {BEARER_TOKEN}"
+#     }
+#     #url: str = f"{GENAI_DATA_ASK_API_ENDPOINT}/ask"
+#     url: str = f"{GENAI_DATA_ASK_API_ENDPOINT}/ask?structured=true"
+#     response: requests.Response = requests.post(url=url, headers=headers, json=payload)
+#     status_code = response.status_code
+#     content = response.json()
+#     print(f"status_code: {status_code}")
+#     print(f"content:")
+#     print(json.dumps(content, indent=4))
+#     ask = content.get('answer')
+#     return Answer(**ask)
 
-def split_response(input_text: str, history: List):
-    history = history or []
-    response = chatbot(input_text)
-    # print(response)
-    # if "(Sources:" in response:
-    #     answer_response, sources = response.split("(Sources:", 1)
-    #     print(answer_response)
-    #     sources = "(Sources: " + sources
-    #     print(sources)
-    # elif "(source:" in response:
-    #     answer_response, sources = response.split("(source:", 1)
-    #     print(answer_response)
-    #     sources = "(source: " + sources
-    #     print(sources)
-    # else: 
-    #     answer_response, sources = response, ""
+# def create_payload(question: str) -> Dict[str, Any]:
+
+#     payload: Dict[str, Any] = {
+#         "ask": {
+#             "question": question
+#         }
+#     }
+#     return payload
+
+
+# def ask(query: str):
+#     payload = create_payload(query)
+#     response: Answer = dispatch_payload(payload)
+#     metadata: List[ChunkWithMetadata] = response.metadata
+#     # output = ''
+#     # for m in metadata:
+#     #     output = output + str(m) + "\n"
+#     #     #m.metadata.<author....>
+#     print(response.content)
+#     #print(output)
+#     return response.content
+
+# def split_response(input_text: str, history: List):
+#     history = history or []
+#     response = chatbot(input_text)
+#     # print(response)
+#     # if "(Sources:" in response:
+#     #     answer_response, sources = response.split("(Sources:", 1)
+#     #     print(answer_response)
+#     #     sources = "(Sources: " + sources
+#     #     print(sources)
+#     # elif "(source:" in response:
+#     #     answer_response, sources = response.split("(source:", 1)
+#     #     print(answer_response)
+#     #     sources = "(source: " + sources
+#     #     print(sources)
+#     # else: 
+#     #     answer_response, sources = response, ""
         
-    # full_response = f"{input_text}\n\n{answer_response}\n\n"
+#     # full_response = f"{input_text}\n\n{answer_response}\n\n"
     
-    #history.append((input_text, answer_response))
+#     #history.append((input_text, answer_response))
     
-    print(response)
+#     print(response)
     
-    #return history, history
-    #return answer_response
-    return response
+#     #return history, history
+#     #return answer_response
+#     return response
 
 def chatbot(conversation):
-    new_message = ask(conversation)
+    new_message = "using a hardcoded string" #ask(conversation)
     #return "User: " + conversation + "\n\nSystem: " + new_message + "\n\n"
     return new_message
 
@@ -152,12 +152,12 @@ if __name__ == "__main__":
         msg = gr.Textbox()
         clear = gr.ClearButton([msg, chat])
         
-        def respond(message, chat_history):
-            bot_message = split_response(message, chat_history)
-            chat_history.append((message, bot_message))
-            return "", chat_history
+        # def respond(message, chat_history):
+        #     bot_message = split_response(message, chat_history)
+        #     chat_history.append((message, bot_message))
+        #     return "", chat_history
         
-        msg.submit(respond, [msg, chat], [msg, chat])
+        # msg.submit(respond, [msg, chat], [msg, chat])
         demo.launch()
     
     #while True:
